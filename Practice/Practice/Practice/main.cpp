a803134984d9c7c334aeb9ea7947eb8523a0584a
DESKTOP-COA1C72\AR student <test@gmail.com>
2024-02-26 19:36:54 +0900

    단축키

--- Practice/Practice/Practice/main.cpp
+++ Practice/Practice/Practice/main.cpp
@@ -3,8 +3,16 @@
 int main()
 {
 
-	std::cout << "Hello World\n"; //줄바꿈 \n 이였음!!
-	std::cout << "I'm a c++ Program";
+	//std::cout << "hello world\n"; //줄바꿈 \n 이였음!!
+	//std::cout << "i'm a c++ program";
+	//주석처리: ctrl k c 
+	//주석풀기 ctrl k u
+
+	int a = 5;
+	int b = 2;
+	int a = int a + 8;
+	
+	std::cout << a - b;
 
 	
 }
