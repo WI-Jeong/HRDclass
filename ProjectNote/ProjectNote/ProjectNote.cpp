5c51d2d34e5072c47b0f033da1e44afae094b6cf
WI-Jeong <141189979+WI-Jeong@users.noreply.github.com>
2024-03-15 10:06:14 +0900

    랜덤라이브러리 가위바위보

--- ProjectNote/ProjectNote/ProjectNote.cpp
+++ ProjectNote/ProjectNote/ProjectNote.cpp
@@ -2,10 +2,118 @@
 //
 
 #include <iostream>
+#include <random>
+
+enum RSP
+{
+	Exit,
+	SCISSOR,
+	ROCK,
+	PAPER
+};
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dis(1, 3); // Generate random numbers between 1 and 3
+
+    int playerwin = 0;
+    int aiwin = 0;
+    int nobodywin = 0;
+
+    while (true)
+    {
+        system("cls");
+
+        std::cout << "1.가위\n2.바위\n3.보\n0. 종료" << std::endl;
+        std::cout << "가위, 바위, 보 중 하나를 입력하세요: " << std::endl;
+        int player = 0;
+        std::cin >> player;
+
+        if (player == SCISSOR)
+            std::cout << "Player: 가위 " << std::endl;
+        else if (player == ROCK)
+            std::cout << "Player: 바위 " << std::endl;
+        else if (player == PAPER)
+            std::cout << "Player: 보 " << std::endl;
+        else if (player == Exit)
+        {
+            int yn = 0;
+            std::cout << "종료하시겠습니까?\n1.Y\n2.N." << std::endl;
+            std::cin >> yn;
+            if (yn == 1)
+            {
+                std::cout << "종료합니다." << std::endl;
+                break;
+            }
+            if (yn == 2)
+            {
+                std::cout << "다시 시작합니다." << std::endl;
+                system("pause");
+                continue;
+            }
+        }
+        else if (player > 3 || player < 1)
+        {
+            std::cout << "다시 입력하세요" << std::endl;
+            system("pause");
+            continue;
+        }
+
+        int ai = dis(gen);
+
+        switch (ai)
+        {
+        case SCISSOR:
+            std::cout << "AI: 가위" << std::endl;
+            break;
+        case ROCK:
+            std::cout << "AI: 바위" << std::endl;
+            break;
+        default:
+            std::cout << "AI: 보" << std::endl;
+            break;
+        }
+
+        int result = player - ai;
+
+        std::cout << std::endl;
+
+        if (result == -2 || result == 1)
+        {
+            std::cout << "플레이어 승리" << std::endl;
+            ++playerwin;
+        }
+        else if (result == 0)
+        {
+            std::cout << "무승부" << std::endl;
+            ++nobodywin;
+        }
+        else
+        {
+            std::cout << "AI 승리" << std::endl;
+            ++aiwin;
+        }
+
+        std::cout << std::endl;
+
+        std::cout << "플레이어 승리 횟수: " << playerwin << std::endl;
+        std::cout << "AI 승리 횟수: " << aiwin << std::endl;
+        std::cout << "무승부 횟수: " << nobodywin << std::endl;
+
+        if (playerwin >= 5)
+        {
+            std::cout << std::endl;
+            std::cout << "플레이어가 5회 이상 이겼습니다.\n자동으로 종료합니다." << std::endl;
+            break;
+        }
+
+        system("pause");
+    }
+
+    return 0;
 }
 
 // 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
