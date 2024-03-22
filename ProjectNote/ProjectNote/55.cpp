79eb8140bcd51a91a42c17f443ab568d30901a16
WI-Jeong <141189979+WI-Jeong@users.noreply.github.com>
2024-03-22 13:39:55 +0900

    정적변수

--- ProjectNote/ProjectNote/55.cpp
+++ ProjectNote/ProjectNote/55.cpp
@@ -1,8 +1,31 @@
 #include <iostream>
 #include "func.h"
+#include "general.h"
+
+int g_i = 0;
+static int gstatic = 0;
+
+int Test()
+{
+	static int i = 0;
+	//++g_i;
+	++i;
+	
+	return i;
+}
 
 int main()
 {
 	int i = Mul(10, 20);
+
+	Test();
+	Test();
+	Test();
+	Test();
+	int icallcount=Test();
+
+	//g_i = 0;
+
+	printf("Test함수 호출 횟수: %d\n", icallcount);
 
 }
--- ProjectNote/ProjectNote/func.cpp
+++ ProjectNote/ProjectNote/func.cpp
@@ -1,18 +1,18 @@
 #include "func.h"
+#include "general.h"
 
-int Add(int a, int b);
+
+int Add(int a, int b)
 {
 	return a + b;
 }
 
-int Sub(int a, int b);
+int Sub(int a, int b)
 {
 	return a - b;
 }
 
-int Mul(int a, int b);
+int Mul(int a, int b)
 {
-	return a * b;
+	return a* b;
 }
-
-
