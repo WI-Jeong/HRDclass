86bb14ed4fa27abe6b80f689d226e72062142a20
WI-Jeong <141189979+WI-Jeong@users.noreply.github.com>
2024-03-24 23:59:17 +0900

    Update Main.cpp

--- CTest/CTest/Main.cpp
+++ CTest/CTest/Main.cpp
@@ -2,7 +2,35 @@
 #define PLUS(Value0, Value1) Value0 + Value1
 #define MUL (Value0, Value1) Value0 * Value1
 
+class A
+{
+public:
+	A()
+	{
+		std::cout << "A";
+		Function();
+	}
+	virtual void Function()
+	{
+		std::cout << "AF";
+	}
+};
+
+class B :public A
+{
+public:
+	B()
+	{
+		std::cout << "B";
+		Function();
+	}
+	 void Function() override
+	{
+		std::cout << "BF";
+	}
+};
+
 int main()
 {
-	int Value = MUL(PLUS(10, 10), PLUS(10, 10));
+	B newB = B();
 }
