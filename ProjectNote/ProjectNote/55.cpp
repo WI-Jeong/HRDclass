faf134e293a8493c2247bbdc51804f07c91d9b0f
WI-Jeong <141189979+WI-Jeong@users.noreply.github.com>
2024-03-28 16:21:41 +0900

    문자열

--- ProjectNote/ProjectNote/55.cpp
+++ ProjectNote/ProjectNote/55.cpp
@@ -20,9 +20,38 @@ void Test(float a)
 
 }
 
+
+unsigned int GetLength(const wchar_t* _pStr)
+{
+	//문자 개수 체크 용도
+	int i = 0;
+
+	while (true)
+	{
+		wchar_t c = _pStr[i];
+
+		if ('\0' == c)
+		{
+			break;
+		}
+
+		++i;
+	}
+
+	return i;
+}
+
 void StrCat(wchar_t* _pDest, unsigned int _iSize, const wchar_t* _pSrc)
 {
-	assert(nullptr);
+	int iDestLen = GetLength(_pDest);
+	int iSrcLen = GetLength(_pSrc);
+
+	if (_iSize < iDestLen + iSrcLen + 1)
+	{
+		//iDestLen + iSrcLen + 1=> NULL문자 공간까지 계산
+		assert(nullptr);
+	}
+
 }
 
 
