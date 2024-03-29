9146b7535b93141fa605791d25e8248fe127504b
WI-Jeong <141189979+WI-Jeong@users.noreply.github.com>
2024-03-29 17:12:25 +0900

    태양 C++ 생성

---
+++ UnrealEngine/HRDKDT2/Source/HRDKDT2/Actors/SolarSystem/Sun.cpp
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Actors/SolarSystem/Sun.h"
+
+// Sets default values
+ASun::ASun()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	bool bCDO=HasAnyFlags(EObjectFlags::RF_ClassDefaultObject);
+
+	StaticMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StaticMesh"));
+	SetRootComponent(StaticMeshComponent);
+
+	{
+		static ConstructorHelpers::FObjectFinder<UStaticMesh> ObjectFinder(TEXT("/Script/Engine.StaticMesh'/Engine/BasicShapes/Sphere.Sphere'"));
+		ensure(ObjectFinder.Object);
+		StaticMeshComponent->SetStaticMesh(ObjectFinder.Object);
+	}
+	{
+		static ConstructorHelpers::FObjectFinder<UMaterial> ObjectFinder(TEXT("/Script/Engine.Material'/Game/KDT2/SolarSystem/MT_Sun.MT_Sun'"));
+		ensure(ObjectFinder.Object);
+		StaticMeshComponent->SetMaterial(0,ObjectFinder.Object);
+	}
+
+}
+
+// Called when the game starts or when spawned
+void ASun::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ASun::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
