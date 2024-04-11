c0dd070da75a60dc1f68b373febba1547969a8db
WI-Jeong <141189979+WI-Jeong@users.noreply.github.com>
2024-04-11 11:51:12 +0900

    Update 55.cpp

--- ProjectNote/ProjectNote/55.cpp
+++ ProjectNote/ProjectNote/55.cpp
@@ -7,8 +7,11 @@
 
 int main()
 {
-	int number = 100;
-	std::cout << ++number << std::endl;
-	std::cout << number++ << std::endl;
+	const int iattack = 0x0000001;
+	const int iarmor = 0x0000002;
+	const int ihp = 0x0000004;
+	const int imp = 0x0000008;
+	const int icritical = 0x00000010;
 
+	int ibuf = iattack | ihp | icritical;
 }
