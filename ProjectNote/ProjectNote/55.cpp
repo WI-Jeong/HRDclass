793f3e5fa506b2f1250694d639da9a6800dce3be
WI-Jeong <141189979+WI-Jeong@users.noreply.github.com>
2024-04-11 11:30:19 +0900

    Update 55.cpp

--- ProjectNote/ProjectNote/55.cpp
+++ ProjectNote/ProjectNote/55.cpp
@@ -7,6 +7,16 @@
 
 int main()
 {
-	int a = 123;
-	std::cout << typeid(a).name() << std::endl;
+	int i{ 1 };
+	int k{ 2 };
+
+	i += k;	//i=i+k=1+2= 3
+	i += 2;	//i=5
+	i -= 3;	//i=2
+	i *= 2;	//i=2*2=4
+	i /= 2;	//i=2
+	i %= 2;	//i=0
+
+	int n = 4;
+	n /= 3; // n=1
 }
