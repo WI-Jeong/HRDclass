1240acd6f70acd8277d142e52fa6cfd930875550
WI-Jeong <141189979+WI-Jeong@users.noreply.github.com>
2024-04-11 17:03:29 +0900

    열거체

--- ProjectNote/ProjectNote/55.cpp
+++ ProjectNote/ProjectNote/55.cpp
@@ -3,24 +3,51 @@
 #include <format>
 #include "func.h"
 #include "general.h"
-#define HUNGRY 1
-#define THIRSTY 2
-#define TIRED 4
+
+
+enum ETier : unsigned char
+{
+	None,
+	Iron,
+	Bronze,
+	Silver,
+	Gold
+};
+/*
+
+		enum ESeason
+	{
+		Spring = 1,
+		Summer,
+		Fall,
+		Winter
+	};
+
+	ESeason Birthday = ESeason::Fall;
+	std::cout << "생일이 속한 계절= " << Birthday << std::endl;
+
+	이거 값 3나옴.
+
+		enum ESeason : unsigned char
+	{
+		Spring = 1,
+		Summer,
+		Fall,
+		Winter
+	};
+
+	ESeason Birthday = ESeason::Fall;
+	std::cout << "생일이 속한 계절= " << Birthday << std::endl;
+
+	이거 값 아무것도 안나옴
+
+		나오게 하려면
+		std::cout << "생일이 속한 계절= " << (int)Birthday << std::endl;
+	로 int로 형변환해서 구해줘야함.
+*/
 
 
 int main()
 {
-	unsigned int iStatus = 0;
-	iStatus |= HUNGRY;
-	iStatus |= THIRSTY;
 
-	iStatus &= ~THIRSTY;
-
-	if (iStatus & THIRSTY)
-	{
-		std::cout << "THIRSTY 상태입니다.\n";
-	}
-
-	else
-		std::cout << "THIRSTY 상태가 아닙니다.\n";
 }
