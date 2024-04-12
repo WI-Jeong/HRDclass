3ec2b373807e1675871bd77bc4b8eb3c8f9f4b25
WI-Jeong <141189979+WI-Jeong@users.noreply.github.com>
2024-04-12 11:32:38 +0900

    Update 55.cpp

--- ProjectNote/ProjectNote/55.cpp
+++ ProjectNote/ProjectNote/55.cpp
@@ -5,49 +5,19 @@
 #include "general.h"
 
 
-enum ETier : unsigned char
+enum EENUM
 {
-	None,
-	Iron,
-	Bronze,
-	Silver,
-	Gold
+	num = 10,
+	num1,
+	num2 = 77,
+	num3
 };
-/*
-
-		enum ESeason
-	{
-		Spring = 1,
-		Summer,
-		Fall,
-		Winter
-	};
-
-	ESeason Birthday = ESeason::Fall;
-	std::cout << "생일이 속한 계절= " << Birthday << std::endl;
-
-	이거 값 3나옴.
-
-		enum ESeason : unsigned char
-	{
-		Spring = 1,
-		Summer,
-		Fall,
-		Winter
-	};
-
-	ESeason Birthday = ESeason::Fall;
-	std::cout << "생일이 속한 계절= " << Birthday << std::endl;
-
-	이거 값 아무것도 안나옴
-
-		나오게 하려면
-		std::cout << "생일이 속한 계절= " << (int)Birthday << std::endl;
-	로 int로 형변환해서 구해줘야함.
-*/
 
 
 int main()
 {
+	std::cout << sizeof(EENUM) << std::endl;
+	std::cout << typeid(EENUM).name() << std::endl;
 
+	EENUM::num1;
 }
