cb4cf4f0dea9e0620c706eeeac300c85134d68f1
WI-Jeong <141189979+WI-Jeong@users.noreply.github.com>
2024-04-12 16:33:19 +0900

    Update 55.cpp

--- ProjectNote/ProjectNote/55.cpp
+++ ProjectNote/ProjectNote/55.cpp
@@ -5,36 +5,11 @@
 #include "general.h"
 
 
-enum ESeason
-{
-	Spring = 1,
-	Summer,
-	Fall,
-	Winter
-};
-
 
 int main()
 {
 
-	ESeason Birthday = ESeason::Fall;
-
-	switch (Birthday)
-	{
-	case Spring:
-		std::cout << "생일이 속한 계절은 봄 입니다." << std::endl;
-		break;
-
-	case Summer:
-		std::cout << "생일이 속한 계절은 여름 입니다." << std::endl;
-		break;
-
-	case Fall:
-		std::cout << "생일이 속한 계절은 가을 입니다." << std::endl;
-		break;
-
-	default:
-		std::cout << "생일이 속한 계절은 봄 입니다." << std::endl;
-		break;
-	}
 }
+
+
+
