f3755c2cf8123c7dd1a4738a7182fcbba56ad836
WI-Jeong <141189979+WI-Jeong@users.noreply.github.com>
2024-04-16 17:32:54 +0900

    Update 55.cpp

--- ProjectNote/ProjectNote/55.cpp
+++ ProjectNote/ProjectNote/55.cpp
@@ -3,9 +3,51 @@
 #define NAME_SIZE 32
 
 
+enum class ETier : unsigned char
+{
+	None,
+	Iron,
+	Bronze,
+	Silver,
+	Gold,
+};
+
+struct FPlayer
+{
+	int HP = 100;
+	int MP{ 10 };
+	ETier Tier = ETier::None;
+};
+
 int main()
 {
-	int value = 0;
-	std::cin >> value;
-	std::cout << std::format("Value값은: {}\n", value);
+	FPlayer Player;
+	Player.Tier = ETier::Gold;
+	std::cout << "[Player Info]\n";
+	std::cout << std::format("HP: {}, MP: {}\n", Player.HP, Player.MP);
+	//std::cout << std::format("Tier: {}\n", (int)Player.Tier);
+
+	switch (Player.Tier)
+	{
+	case ETier::None:
+		std::cout << "Tier: None\n";
+		break;
+	case ETier::Iron:
+		std::cout << "Tier: Iron\n";
+		break;
+	case ETier::Bronze:
+		std::cout << "Tier: Bronze\n";
+		break;
+	case ETier::Silver:
+		std::cout << "Tier: Silver\n";
+		break;
+	case ETier::Gold:
+		std::cout << "Tier: Gold\n";
+		break;
+	default:
+		std::cout << "default\n";
+		break;
+	}
 }
+
+
