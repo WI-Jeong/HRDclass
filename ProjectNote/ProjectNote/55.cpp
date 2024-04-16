9942fdf0248a7fa4a4e446ba01671165baccaa7b
WI-Jeong <141189979+WI-Jeong@users.noreply.github.com>
2024-04-16 15:18:28 +0900

    Update 55.cpp

--- ProjectNote/ProjectNote/55.cpp
+++ ProjectNote/ProjectNote/55.cpp
@@ -2,51 +2,10 @@
 #include <format>
 #define NAME_SIZE 32
 
-struct player
-{
-	char name[NAME_SIZE];
-	int i;
-	int j;
-	struct startest;
-};
-
-struct FPadding
-{
-	char C;
-
-};
-
-struct FPadding2
-{
-	char C;
-	int I;
-};
-
-struct FPadding3
-{
-	int I;
-	char C;
-	char C2; 
-	
-};
-
-
 
 int main()
 {
-	std::cout << sizeof(FPadding3);
-
-	```
-		if (true)
-		{
-			std::cout << "If문 조건이 true입니다." << std::endl;
-		}
-	-> 출력됨.
-
-		if (false)
-		{
-			std::cout << "If문 조건이 false입니다." << std::endl;
-		}
-	->if문의 조건식이 false이므로 출력되지 않는다.
-		```
+	int value = 0;
+	std::cin >> value;
+	std::cout << std::format("Value값은: {}\n", value);
 }
