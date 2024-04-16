9acd52050e26e8b3621ba8516eee74ae725439f0
WI-Jeong <141189979+WI-Jeong@users.noreply.github.com>
2024-04-16 13:22:34 +0900

    Update 55.cpp

--- ProjectNote/ProjectNote/55.cpp
+++ ProjectNote/ProjectNote/55.cpp
@@ -35,4 +35,18 @@ struct FPadding3
 int main()
 {
 	std::cout << sizeof(FPadding3);
+
+	```
+		if (true)
+		{
+			std::cout << "If문 조건이 true입니다." << std::endl;
+		}
+	-> 출력됨.
+
+		if (false)
+		{
+			std::cout << "If문 조건이 false입니다." << std::endl;
+		}
+	->if문의 조건식이 false이므로 출력되지 않는다.
+		```
 }
