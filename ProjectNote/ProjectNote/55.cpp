17fd8ae0689a1dc0c5f906e103cdd311aad627a0
WI-Jeong <141189979+WI-Jeong@users.noreply.github.com>
2024-04-17 17:01:35 +0900

    강화코드 짜는 중

--- ProjectNote/ProjectNote/55.cpp
+++ ProjectNote/ProjectNote/55.cpp
@@ -1,29 +1,35 @@
 #include <iostream>
 #include <format>
-#define NAME_SIZE 32
+#include<time.h>
 
 
-enum class ETier : unsigned char
-{
-	None,
-	Iron,
-	Bronze,
-	Silver,
-	Gold,
-};
+/*
+업그레이드가 0~3일 경우 강화 100% 성공
+업그레이드가 4~6일 경우 강화 40% 성공
+업그레이드가 7~9일 경우 강화 10% 성공
+업그레이드가 10~13일 경우 강화 1% 성공
+업그레이드가 14~15일 경우 강화 0.01% 성공
+업그레이드 수치는 직접 입력하게 한다.
+*/
 
-struct FPlayer
-{
-	int HP = 100;
-	int MP{ 10 };
-	ETier Tier = ETier::None;
-};
 
 int main()
 {
-	FPlayer Player;
-	Player.Tier = ETier::Gold;
+	srand((unsigned int)time(0));
+	rand();
 
+	std::cout << "업그레이드 수치를 입력하세요: \n";
+
+	int Upgrade = 0;
+	float Percent = rand() % 10000 / 100;
+
+	std::cin >> Upgrade;
+
+
+	std::cout << std::format("업그레이드 수치: {}, 강화확률: {}%",Upgrade,Percent);
+
+
+;
 }
 
 
