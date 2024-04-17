b377884da0357f57c77bea546529747bfbcbfac7
WI-Jeong <141189979+WI-Jeong@users.noreply.github.com>
2024-04-17 16:12:17 +0900

    Update 55.cpp

--- ProjectNote/ProjectNote/55.cpp
+++ ProjectNote/ProjectNote/55.cpp
@@ -23,31 +23,8 @@ int main()
 {
 	FPlayer Player;
 	Player.Tier = ETier::Gold;
-	std::cout << "[Player Info]\n";
-	std::cout << std::format("HP: {}, MP: {}\n", Player.HP, Player.MP);
-	//std::cout << std::format("Tier: {}\n", (int)Player.Tier);
 
-	switch (Player.Tier)
-	{
-	case ETier::None:
-		std::cout << "Tier: None\n";
-		break;
-	case ETier::Iron:
-		std::cout << "Tier: Iron\n";
-		break;
-	case ETier::Bronze:
-		std::cout << "Tier: Bronze\n";
-		break;
-	case ETier::Silver:
-		std::cout << "Tier: Silver\n";
-		break;
-	case ETier::Gold:
-		std::cout << "Tier: Gold\n";
-		break;
-	default:
-		std::cout << "default\n";
-		break;
-	}
 }
+
 
 
