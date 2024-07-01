95c068eedc0d03b4ce2e467dcd719fc75390625d
WI-Jeong <141189979+WI-Jeong@users.noreply.github.com>
2024-07-01 16:25:40 +0900

    - Tempate

---
+++ Server/Engine/Templates/ProjectTemplates/Game/Source/MyProject/ProjectGameEngine.cpp
@@ -0,0 +1,11 @@
+﻿#include "ProjectGameEngine.h"
+
+UProjectGameEngine::UProjectGameEngine()
+{
+
+}
+
+UProjectGameEngine::~UProjectGameEngine()
+{
+	
+}
---
+++ Server/Engine/Templates/ProjectTemplates/Game/Source/MyProject/ProjectGameEngine.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+// AssortRock Inc.
+
+#include "EngineMinimal.h"
+#include "ProjectGameEngine.generated.h"
+
+UCLASS()
+class GAMEPROJECT_API UProjectGameEngine : public UGameEngine
+{
+	GENERATED_BODY();
+
+public:
+	UProjectGameEngine();
+	~UProjectGameEngine();
+};
