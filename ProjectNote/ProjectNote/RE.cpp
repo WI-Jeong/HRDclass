ad1df82d9288a6f593959310c789298d7beb60f9
WI-Jeong <141189979+WI-Jeong@users.noreply.github.com>
2024-07-04 17:38:35 +0900

    thread

--- ProjectNote/ProjectNote/RE.cpp
+++ ProjectNote/ProjectNote/RE.cpp
@@ -1,19 +1,25 @@
 #include <stdio.h>
 #include <iostream>
+#include <thread>
 using namespace std;
 
-void function(int count)
+void thread_function(int count)
 {
 	for (int i = 0; i < count; i++)
 	{
-		cout << " i / count  = " << i << " / " << count << endl;
+		cout << " i - " << i << endl;
+		this_thread::sleep_for(chrono::milliseconds(10));
 	}
-
 }
 
 int main()
 {
-	function(10);
-	function(10);
+	thread _t1(thread_function, 10);
+
+	thread _t2(thread_function, 10);
+	cout << "main process" << endl;
+
+	_t1.join();
+	_t2.join();
 	return 0;
 }
